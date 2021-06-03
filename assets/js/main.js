d08020e4937ee5315482bd13a8342ef61f9742dc
Ivan Rosa <iivan.si@msn.com>
2021-06-03 19:21:33 -0300

    Ajuste na animação 'Hour Assistant Animation'

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 // 'btn' Click to animate the hour assistant above
 const canAnimate = document.querySelector('#canAnimate');
+const divCheck = document.querySelector('#divCheck');
 
 // the assistant element
 const hourAssistant = document.querySelector('.hour-assistant');
@@ -8,7 +9,10 @@ const hourAssistant = document.querySelector('.hour-assistant');
 const optionCanAnimate = document.querySelector('.optionCanAnimate');
 
 // active/disable the animations
-canAnimate.addEventListener('click', () => {
+canAnimate.addEventListener('click', toggleAnimation);
+divCheck.addEventListener('click', toggleAnimation);
+
+function toggleAnimation(){
     hourAssistant.classList.toggle('pauseAnimation');
     optionCanAnimate.classList.toggle('active');
-});
+}

58f477323e0905b35384497b27467524b96e832e
Ivan Rosa <iivan.si@msn.com>
2021-06-06 02:05:47 -0300

    Seção 'Calendar' refeito

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,16 @@
 // 'btn' Click to animate the hour assistant above
-const canAnimate = document.querySelector('#canAnimate');
-const divCheck = document.querySelector('#divCheck');
+const lCanAnimate = document.querySelector('.lCanAnimate');
+const divCheck = document.querySelector('.divCheck');
+
+// active/disable the animations
+lCanAnimate.addEventListener('click', toggleAnimation);
+divCheck.addEventListener('click', toggleAnimation);
 
 // the assistant element
-const hourAssistant = document.querySelector('.hour-assistant');
+const hourAssistant = document.querySelector('.hourAssistant');
 
 // the checkbox label than show the message 'HOUR ASSISTANT ANIMATION'
 const optionCanAnimate = document.querySelector('.optionCanAnimate');
-
-// active/disable the animations
-canAnimate.addEventListener('click', toggleAnimation);
-divCheck.addEventListener('click', toggleAnimation);
 
 function toggleAnimation(){
     hourAssistant.classList.toggle('pauseAnimation');
